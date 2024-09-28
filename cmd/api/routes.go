d2fa3212af7221165f28ec1d93783c490e6c1118
santiagobedoa <santiagobedoa@gmail.com>
2024-09-28 16:32:28 -0500

    chapter 3.4

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -15,7 +15,7 @@ func (app *application) routes() *httprouter.Router {
 	// respectively.
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
-	router.HandlerFunc(http.MethodGet, "/v1/movies", app.showMovieHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
 
 	// Return the httprouter instance.
 	return router
--- internal/data/movies.go
+++ internal/data/movies.go
@@ -4,13 +4,14 @@ import (
 	"time"
 )
 
+// Annotate the Movie struct with struct tags to control how the keys appear in the
+// JSON-encoded output.
 type Movie struct {
-	ID        int64     // Unique integer ID for the movie
-	CreatedAt time.Time // Timestamp for when the movie is added to our database
-	Title     string    // Movie title
-	Year      int32     // Movie release year
-	Runtime   int32     // Movie runtime (in minutes)
-	Genres    []string  // Slice of genres for the movie (romance, comedy, etc.)
-	Version   int32     // The version number starts at 1 and will be incremented each
-	// time the movie information is updated
+	ID        int64     `json:"id"`
+	CreatedAt time.Time `json:"-"`
+	Title     string    `json:"title"`
+	Year      int32     `json:"year,omitempty"`
+	Runtime   int32     `json:"runtime,omitempty"`
+	Genres    []string  `json:"genres,omitempty"`
+	Version   int32     `json:"version"`
 }
