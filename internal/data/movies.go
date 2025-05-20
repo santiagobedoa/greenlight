469d0ce01ca1d2208ed27e1f85f1acb08008662d
santiagobedoa <santiagobedoa@gmail.com>
2025-05-20 09:21:54 -0500

    chapter 9.6

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -144,35 +144,25 @@ func (m MovieModel) Delete(id int64) error {
 	return nil
 }
 
-// Create a new GetAll() method which returns a slice of movies. Although we're not
-// using them right now, we've set this up to accept the various filter parameters as
-// arguments.
 func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*Movie, error) {
-	// Construct the SQL query to retrieve all movie records.
 	query := `
-SELECT id, created_at, title, year, runtime, genres, version
-FROM movies
-ORDER BY id`
-	// Create a context with a 3-second timeout.
+		SELECT id, created_at, title, year, runtime, genres, version
+		FROM movies
+		WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) OR $1 = '')
+		AND (genres @> $2 OR $2 = '{}')
+		ORDER BY id`
+
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
-	// Use QueryContext() to execute the query. This returns a sql.Rows resultset
-	// containing the result.
-	rows, err := m.DB.QueryContext(ctx, query)
+	// Pass the title and genres as the placeholder parameter values.
+	rows, err := m.DB.QueryContext(ctx, query, title, pq.Array(genres))
 	if err != nil {
 		return nil, err
 	}
-	// Importantly, defer a call to rows.Close() to ensure that the resultset is closed
-	// before GetAll() returns.
 	defer rows.Close()
-	// Initialize an empty slice to hold the movie data.
 	movies := []*Movie{}
-	// Use rows.Next to iterate through the rows in the resultset.
 	for rows.Next() {
-		// Initialize an empty Movie struct to hold the data for an individual movie.
 		var movie Movie
-		// Scan the values from the row into the Movie struct. Again, note that we're
-		// using the pq.Array() adapter on the genres field here.
 		err := rows.Scan(
 			&movie.ID,
 			&movie.CreatedAt,
@@ -185,14 +175,10 @@ ORDER BY id`
 		if err != nil {
 			return nil, err
 		}
-		// Add the Movie struct to the slice.
 		movies = append(movies, &movie)
 	}
-	// When the rows.Next() loop has finished, call rows.Err() to retrieve any error
-	// that was encountered during the iteration.
 	if err = rows.Err(); err != nil {
 		return nil, err
 	}
-	// If everything went OK, then return the slice of movies.
 	return movies, nil
 }
